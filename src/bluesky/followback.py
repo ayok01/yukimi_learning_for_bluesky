ce328cb1fc8ac7f420c9f696bdd9935284b0cc27
neruneru <neruneru55555@gmail.com>
2024-02-16 23:46:34 +0900

    config.jsonから値を取得

--- src/bluesky/followback.py
+++ src/bluesky/followback.py
@@ -1,8 +1,13 @@
 from atproto import Client
+import json
+
+with open('../config.json', 'r') as json_file:
+    config = json.load(json_file)
 
 client = Client()
-user_name = "XXXXXXXXX"
-password = "XXXXXXXX"
+user_name = config["login_data"]["user_name"]
+password = config["login_data"]["password"]
+
 client.login(user_name, password)
 
 def follow_back():
--- src/bluesky/post.py
+++
@@ -1,9 +0,0 @@
-#!python3.8
-from atproto import Client
-
-client = Client()
-user_name = "neruneru0419.bsky.social"
-password = "XXXXXXXXXXXXX"
-client.login(user_name, password)
-
-client.send_post(text='APIからポスト')
