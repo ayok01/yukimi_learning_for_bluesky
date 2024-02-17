0eb12fb137eafbcac49ffc053ec268f903d2db20
neruneru <neruneru55555@gmail.com>
2024-02-17 20:10:52 +0900

    config.jsonから値を取る

--- src/bluesky/post.py
+++ src/bluesky/post.py
@@ -1,9 +1,13 @@
 #!python3.8
 from atproto import Client
+import json
+
+with open('../config.json', 'r') as json_file:
+    config = json.load(json_file)
 
 client = Client()
-user_name = "neruneru0419.bsky.social"
-password = "XXXXXXXXXXXXX"
+user_name = config["login_data"]["user_name"]
+password = config["login_data"]["password"]
 client.login(user_name, password)
 
 client.send_post(text='APIからポスト')
