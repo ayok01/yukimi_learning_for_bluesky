c03320c92056f1c6dcbed6841c852afb79438cb5
neruneru <neruneru55555@gmail.com>
2024-02-17 23:01:09 +0900

    ngword_filterをimport

--- src/bluesky/get_timeline.py
+++ src/bluesky/get_timeline.py
@@ -1,4 +1,5 @@
 from atproto import Client
+from ngword_filter import judgement_sentence
 import json
 
 with open('../config.json', 'r') as json_file:
@@ -16,7 +17,7 @@ def get_timeline():
         text = gt.post.record.text
         handle = gt.post.author.handle
         is_ngword = judgement_sentence(text)
-        if text and handle != user_name and is_ngword:
+        if text and handle != user_name and not is_ngword:
             post_list.append(gt)
     return post_list
 
